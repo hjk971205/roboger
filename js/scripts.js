365378a23280283990c3ff8f34a67c51a4225ca0
Hyung Jin Kim <hyungjinkim1205@gmail.com>
2023-03-31 13:37:08 -0700

    add the function for the number 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,24 @@
 //Business Logic
 function beep(inputNumber, inputName) {
   if (inputNumber < 1) {
-      const paragraph = document.createElement("p");
-      paragraph.append("Please enter a number > 0");
-      document.body.append(paragraph);
+    const paragraph = document.createElement("p");
+    paragraph.append("Please enter a number > 0");
+    document.body.append(paragraph);
   } else {
-      let numberArray = [];
-      for (let i = 0; i <= inputNumber; i++) {
-        if (i.toString().indexOf("2") !== -1) {
-          numberArray.push(" Boop!");  
-        } else if (i.toString().indexOf("1") !== -1) {
-              numberArray.push(" Beep!");
-          } else {
-              numberArray.push(i);
-          }
+    let numberArray = [];
+    for (let i = 0; i <= inputNumber; i++) {
+      if (i.toString().indexOf("3") !== -1) {
+        numberArray.push(" Won't you be my neighbor, " + inputName + "?");
+      } else if (i.toString().indexOf("2") !== -1) {
+        numberArray.push(" Boop!");
+      } else if (i.toString().indexOf("1") !== -1) {
+        numberArray.push(" Beep!");
+      } else {
+        numberArray.push(i);
       }
+    }
 
-      return numberArray;
+    return numberArray;
   }
 }
 
