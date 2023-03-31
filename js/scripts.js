768af6886671000c3e38ae30d8ea4e8208a45476
Hyung Jin Kim <hyungjinkim1205@gmail.com>
2023-03-31 11:09:32 -0700

    add code for numbers with 1

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+function beep(inputNumber, inputName) {
+  if (inputNumber < 1) {
+      const paragraph = document.createElement("p");
+      paragraph.append("Please enter a number > 0");
+      document.body.append(paragraph);
+  } else {
+      let numberArray = [];
+      for (let i = 0; i <= inputNumber; i++) {
+          if (i.toString().indexOf("1") !== -1) {
+              numberArray.push(" Beep!");
+          } else {
+              numberArray.push(i);
+          }
+      }
+
+      return numberArray;
+  }
+}
